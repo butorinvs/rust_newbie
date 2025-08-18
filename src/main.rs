aaea1731999178bf7c3ffa5a7fc7b55d745d26a2
butorinvs <butorinvs@gmail.com>
2025-08-18 21:33:14 +0300

    lint main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,40 @@ fn main() {
     let odin = arr[0] % 10;
 
     if des == 0 && odin == 0 {
-        println!("{}{}{}", sotni, odin, des);
-    } else {
-        if sotni <= des && des <= odin && sotni != 0 {
-            if des <= odin {
-                println!("{}{}{}", sotni, des, odin);
-            } else {
-                println!("{}{}{}", sotni, odin, des);
-            }
-        } else if des <= sotni && des <= odin && des != 0 {
-            if sotni <= odin {
-                println!("{}{}{}", des, sotni, odin);
-            } else {
-                println!("{}{}{}", des, odin, sotni);
-            }
-        } else if odin <= sotni && odin <= des && odin != 0 {
-            if sotni <= des {
-                println!("{}{}{}", odin, sotni, des);
-            } else {
-                println!("{}{}{}", odin, des, sotni);
-            }
+        println!("{sotni}{odin}{des}");
+    } else if sotni <= des && des <= odin && sotni != 0 {
+        if des <= odin {
+            println!("{sotni}{des}{odin}");
         } else {
-            if sotni == 0 {
-                if des <= odin {
-                    println!("{}{}{}", des, sotni, odin);
-                } else {
-                    println!("{}{}{}", odin, sotni, des);
-                }
-            } else if des == 0 {
-                if sotni <= odin {
-                    println!("{}{}{}", sotni, des, odin);
-                } else {
-                    println!("{}{}{}", odin, des, sotni);
-                }
-            } else {
-                if sotni <= des {
-                    println!("{}{}{}", sotni, odin, des);
-                } else {
-                    println!("{}{}{}", des, odin, sotni);
-                }
-            }
+            println!("{sotni}{odin}{des}");
         }
+    } else if des <= sotni && des <= odin && des != 0 {
+        if sotni <= odin {
+            println!("{des}{sotni}{odin}");
+        } else {
+            println!("{des}{odin}{sotni}");
+        }
+    } else if odin <= sotni && odin <= des && odin != 0 {
+        if sotni <= des {
+            println!("{odin}{sotni}{des}");
+        } else {
+            println!("{odin}{des}{sotni}");
+        }
+    } else if sotni == 0 {
+        if des <= odin {
+            println!("{des}{sotni}{odin}");
+        } else {
+            println!("{odin}{sotni}{des}");
+        }
+    } else if des == 0 {
+        if sotni <= odin {
+            println!("{sotni}{des}{odin}");
+        } else {
+            println!("{odin}{des}{sotni}");
+        }
+    } else if sotni <= des {
+        println!("{sotni}{odin}{des}");
+    } else {
+        println!("{des}{odin}{sotni}");
     }
 }
