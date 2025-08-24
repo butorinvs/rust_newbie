9a7bf18aecfca8d763fd94967e836038be540e31
butorinvs <butorinvs@gmail.com>
2025-08-24 11:09:23 +0300

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
         .take(1)
         .map(|x| x.unwrap().trim().parse::<u32>().unwrap())
         .collect::<Vec<_>>();
-    let mut arr= [1, 2, 3, 4, 5];
+    let mut arr = [1, 2, 3, 4, 5];
     arr[0] += v[0] as i32;
     arr[1] -= v[0] as i32;
     arr[2] *= v[0] as i32;
     arr[3] /= v[0] as i32;
     arr[4] %= v[0] as i32;
-    println!("{:?}", arr);
+    println!("{arr:?}");
 }
